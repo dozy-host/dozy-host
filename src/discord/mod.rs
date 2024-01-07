9cbbb1b947c7dd5fb0fdbb602914c510c5a36201
Crystal Keck <meaneea@gmail.com>
2024-01-06 19:28:15 -0600

    Completed DH-3. Start and stop containers

--- src/discord/mod.rs
+++ src/discord/mod.rs
@@ -4,7 +4,9 @@ mod login;
 use anyhow::Error;
 pub use commands::commands;
 
-pub struct Data {} // User data, which is stored and accessible in all command invocations
+pub struct Data {
+    pub docker: bollard::Docker,
+} // User data, which is stored and accessible in all command invocations
 pub type Context<'a> = poise::Context<'a, Data, Error>;
 
 pub use login::login;
